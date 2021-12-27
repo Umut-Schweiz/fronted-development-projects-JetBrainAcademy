ab14d17b7e2efd42ba38a9cfba5bd2a0cfddc748
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-12-28 00:31:08 +0100

    project was styled and completed

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -83,3 +83,13 @@ const clearAll = () => {
     }
     toDoList = [];    
 }
+
+const toggleTheme = (theme) => {
+    const body = document.body;
+    body.classList.remove('dark', 'light')
+    if (theme == 'dark') {
+        body.classList.add('dark')
+    } else {
+        body.classList.add('light')
+    }
+}
